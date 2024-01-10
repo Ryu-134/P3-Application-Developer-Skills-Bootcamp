7508ff4b9c365ac58e5b54b61074322184698369
Ryu-134 <147681738+Ryu-134@users.noreply.github.com>
2024-01-09 23:40:31 -0800

    Version-1.6.7-Minor changes for completing a tournament

--- commands/AdvanceRoundCmd.py
+++ commands/AdvanceRoundCmd.py
@@ -1,8 +1,5 @@
-from commands import ExitCmd
-from commands.GoBackCmd import GoBackCmd
 from commands.RefreshTournamentViewCmd import RefreshTournamentViewCmd
 from commands.context import Context
-
 
 class AdvanceRoundCmd:
     def __init__(self, tournament, club_manager, next_cmd=None):
@@ -11,7 +8,6 @@ class AdvanceRoundCmd:
         self.next_cmd = next_cmd
 
     def execute(self):
-        # Prompt for confirmation to advance to the next round
         confirmation = input("Are you sure you want to advance to the next round? (yes/no): ")
         if confirmation.lower() != 'yes':
             print("Advancing to the next round cancelled.")
@@ -20,18 +16,8 @@ class AdvanceRoundCmd:
         # Check if the current round is the final round and all matches are completed
         if self.tournament.current_round == self.tournament.total_rounds and all(
                 match.completed for match in self.tournament.rounds[-1].matches):
-            print("Tournament completed.")
-            print("Options:")
-            print("1. Go Back to Main Menu")
-            print("2. Exit Application")
-            choice = input("Enter your choice: ")
-            if choice == "1":
-                return GoBackCmd()
-            elif choice == "2":
-                return ExitCmd()
-            else:
-                print("Invalid choice. Returning to main menu.")
-                return GoBackCmd()
+            print("Tournament completed. Exiting application.")
+            exit(0)  # Exit the application
 
         # If it's not the final round, advance to the next round and generate new pairings
         elif self.tournament.can_advance_round():
